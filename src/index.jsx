7736bc9c483c05a888e2e3ed887c29faf740e180
Ted Leary <markeleary@gmail.com>
2018-07-08 17:58:39 -0700

    done for sunday 7/8

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import ticketListReducer from './reducers/ticket-list-reducer';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
-import ticketListReducer from './reducers/ticket-list-reducer';
+
 import { Provider } from 'react-redux';
 
 
